2bfe3ad2a735745beb2f4a762cb4b3a8c6ab20b1
phenry <oluwatoyinferanmijohn@gmail.com>
2023-09-07 17:38:18 +0100

    phenry

--- src/components/feature.vue
+++ src/components/feature.vue
@@ -1,6 +1,120 @@
 <template>
-  <div class="p-5">
-    <img src="../assets/image/image.png" alt="" />
+  <div class="p-5 mx-auto items-center justify-start md:justify-center block md:flex gap-28">
+    <div>
+      <img src="../assets/image/image.png" alt="" />
+    </div>
+    <div>
+      <div class="flex items-center gap-5">
+        <h1 class="new-font text-xl font-normal text-[#ADB5BD] py-3">FEATURES</h1>
+        <div class="flex items-center gap-2 ml-4">
+          <h1 class="new-font text-xl font-normal text-[#ADB5BD] py-3">01</h1>
+          <div class="h-[2px] rounded-full w-40 bg-gradient-to-r from-[#6F9CEB] to-[#F5F5F5]"></div>
+        </div>
+      </div>
+      <div>
+        <h1 class="my-5 text-5xl  text-[#495057] max-w-lg font-semibold capitalize">
+          Ensure that <br />
+          everyone is <br />well-informed.
+        </h1>
+        <p class="max-w-lg">
+          Promote informed collaboration, spotlight payment statuses, and empower efficient action
+          with vital reports and metrics, all within a single solution.
+        </p>
+        <div class="flex gap-3 mt-5">
+          <div>
+            <svg
+              class="w-6 h-6 text-[#6F9CEB]"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 16 12"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5.917 5.724 10.5 15 1.5"
+              />
+            </svg>
+          </div>
+          <p class="max-w-md text-[#6C757D] text-sm">
+            Key reports and metrics aid your team in identifying unpaid individuals for
+            notifications.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div
+    class="py-32 mx-auto items-center justify-start md:justify-center block md:flex md:flex-row-reverse gap-28"
+  >
+    <div>
+      <img src="../assets/image/image2.png" alt="" />
+    </div>
+    <div>
+      <div class="flex flex-row-reverse items-center gap-5">
+        <h1 class="new-font text-xl font-normal text-[#ADB5BD] py-3">FEATURES</h1>
+        <div class="flex flex-row-reverse items-center gap-2 mr-4">
+          <h1 class="new-font text-xl font-normal text-[#ADB5BD] py-3">02</h1>
+          <div class="h-[2px] rounded-full w-40 bg-gradient-to-l from-[#6F9CEB] to-[#F5F5F5]"></div>
+        </div>
+      </div>
+      <div>
+        <h1 class="my-5 text-5xl  text-[#495057] max-w-lg font-semibold text-right capitalize">
+          Efficient <br />receipt <br />
+          issuance, <br />
+          hassle-free.
+        </h1>
+        <p class="max-w-lg text-right">
+          Experience effortless and speedy receipt issuance through our simplified process, ensuring
+          a stress-free and efficient transaction journey.
+        </p>
+        <div class="flex-row-reverse flex gap-3 mt-5">
+          <div>
+            <svg
+              class="w-6 h-6 text-[#6F9CEB]"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 16 12"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5.917 5.724 10.5 15 1.5"
+              />
+            </svg>
+          </div>
+          <p class="max-w-md text-[#6C757D] text-sm">Swift Receipt Generation</p>
+        </div>
+        <div class="flex-row-reverse flex gap-3 mt-5">
+          <div>
+            <svg
+              class="w-6 h-6 text-[#6F9CEB]"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 16 12"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5.917 5.724 10.5 15 1.5"
+              />
+            </svg>
+          </div>
+          <p class="max-w-md text-right text-[#6C757D] text-sm">
+            Simplified Process for a Stress-Free <br />
+            Experience
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
