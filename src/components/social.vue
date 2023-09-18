aa08402f130d80c3386a2fbc54554528c98fd878
phenry <oluwatoyinferanmijohn@gmail.com>
2023-09-18 11:27:58 +0100

    phenry updated

--- src/components/social.vue
+++ src/components/social.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="px-5 py-5 bg-black">
+  <div class="px-5 py-5 bg-black  grid grid-cols-4">
     <div class="m-5">
       <div class="bg-[#171E25] rounded-3xl p-5">
         <div>
           <h1 class="pt-10 text-white text-6xl font-medium">01.</h1>
           <div class="py-5">
-            <img src="../assets/image/laptop.png" alt="" />
+            <img src="../assets/image/desert.png" alt="" />
           </div>
           <div class="text-center text-white">
-            <h1 class="font-semibold text-3xl capitalize">Lorem ipsum.</h1>
+            <h1 class="font-semibold text-xl capitalize">Our foundation relies on open source.</h1>
             <p class="my-5 text-xl font-light">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, reiciendis?
+              You can host your own Plixa in your infrastructure.
             </p>
           </div>
         </div>
@@ -21,12 +21,12 @@
         <div>
           <h1 class="pt-10 text-white text-6xl font-medium">02.</h1>
           <div class="py-5">
-            <img src="../assets/image/laptop.png" alt="" />
+            <img src="../assets/image/desert.png" alt="" />
           </div>
           <div class="text-center text-white">
-            <h1 class="font-semibold text-3xl capitalize">Lorem ipsum.</h1>
+            <h1 class="font-semibold text-xl capitalize">Our foundation relies on open source.</h1>
             <p class="my-5 text-xl font-light">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, reiciendis?
+             You can host your own Plixa in your infrastructure.
             </p>
           </div>
         </div>
