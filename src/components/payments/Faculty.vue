9f6dee2b162931a64d386e15fd0f4a923cb5093f
nobledeveloper01 <nobleconcepts22@gmail.com>
2023-10-16 22:25:40 +0100

    Designed additional 3 screens for Payment feature Plixa

---
+++ src/components/payments/Faculty.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+      <!-- Render the NavViewTwo component -->
+      <NavViewTwo />
+      <!-- Desktop view message -->
+      <div class="md:flex hidden md:text-5xl md:justify-center md:h-screen md:text-center">
+        This page can only be viewed on mobile. Please switch to a mobile device.
+      </div>
+  
+      <!-- Mobile view content -->
+      <div class="mt-20 mx-5">
+        <label for="faculty" class="block text-sm font-medium text-gray-600">Faculty</label>
+        <select
+          id="faculty"
+          v-model="selectedFaculty"
+          placeholder="Select"
+          class="mt-1 p-2 w-full md:w-60 border rounded-md focus:outline-none focus:border-blue-500"
+        >
+          <option value="" disabled selected>Select Faculty</option>
+          <option value="school1">Faculty of Education</option>
+          <option value="school2">Faculty of Science</option>
+          <option value="school3">Faculty of Law</option>
+          
+        </select>
+  
+        <div class="mx-auto my-8 text-center">
+            <router-link to="/Department">
+              <button class="bg-[#001D3D] w-full sm:w-72 py-2 px-8 rounded-xl text-white">
+                Next
+              </button>
+            </router-link>
+          </div>
+      </div>
+    </div>
+  
+    <!-- Render the footer component -->
+    <mainFooter />
+  </template>
+  
+  <script setup>
+  import mainFooter from '../footerTwo.vue'
+  import NavViewTwo from '../../views/navViewTwo.vue'
+  </script>
+  
+  <style scoped>
+  /* Add your scoped styles here */
+  </style>
+  
--- src/components/payments/PaymentOne.vue
+++ src/components/payments/PaymentOne.vue
@@ -8,24 +8,26 @@
     </div>
 
     <!-- Mobile view content -->
-    <div class="md:hidden flex flex-col justify-center items-center text-center mx-auto h-screen py-10 mb-24 relative">
-    <router-link to="/payerDashboard" class="absolute top-4 left-4 sm:hidden">
-      <svg
-        class="w-6 h-6"
-        fill="none"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M10 19l-7-7m0 0l7-7m-7 7h18"
-        ></path>
-      </svg>
-    </router-link>
-      <div class="mt-20">
+    <div
+      class="md:hidden flex flex-col justify-center items-center text-center mx-auto py-10 mb-5 relative"
+    >
+      <router-link to="/payerDashboard" class="absolute top-4 left-4 sm:hidden">
+        <svg
+          class="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M10 19l-7-7m0 0l7-7m-7 7h18"
+          ></path>
+        </svg>
+      </router-link>
+      <div class="mt-5">
         <div class="mx-auto flex justify-center mt-5">
           <img
             src="../../assets/image/Digital-pictures02.png"
@@ -33,24 +35,25 @@
             class="h-96 w-96"
           />
         </div>
-        <h1 class="text-gray-500 text-sm sm:text-sm font-semibold text-center sm:text-center mx-4 mb-5">
+        <h1
+          class="text-gray-500 text-sm sm:text-sm font-semibold text-center sm:text-center mx-4 mb-5"
+        >
           Would you like to make full payment or part payment?
         </h1>
-          <div class="mx-auto my-5 justify-between flex flex-col items-center sm:flex-row">
-            <router-link to="/FullPayment">
-              <button
-                class="bg-[#001D3D] w-72 md:w-auto mb-4 sm:mb-0 md:mb-0 sm:mr-6 py-2 px-8 rounded-xl text-white"
-              >
-                Full Payment
-              </button>
-            </router-link>
-            <router-link to="/PartPayment">
-              <button class="bg-[#001D3D] w-72 md:w-auto py-2 px-8 rounded-xl text-white">
-                Part Payment
-              </button>
-            </router-link>
-          </div>
-        
+        <div class="mx-auto my-5 justify-between flex flex-col items-center sm:flex-row">
+          <router-link to="/FullPayment">
+            <button
+              class="bg-[#001D3D] w-72 md:w-auto mb-4 sm:mb-0 md:mb-0 sm:mr-6 py-2 px-8 rounded-xl text-white"
+            >
+              Full Payment
+            </button>
+          </router-link>
+          <router-link to="/PartPayment">
+            <button class="bg-[#001D3D] w-72 md:w-auto py-2 px-8 rounded-xl text-white">
+              Part Payment
+            </button>
+          </router-link>
+        </div>
       </div>
     </div>
     <footerTwoVue />
