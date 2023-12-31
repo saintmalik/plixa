605bac6251e76740286edc03605685c557cc53f6
nobledeveloper01 <nobleconcepts22@gmail.com>
2023-12-31 15:03:49 +0100

    updated the sponsor

--- src/components/sponsor.vue
+++ src/components/sponsor.vue
@@ -2,8 +2,8 @@
   <div class="flex justify-center py-10 gap-10">
     <div>
   <h1 class="text-xl text-black">Powered by:</h1>
-  <img src="../assets/image/oculus.png" alt="Oculus Logo"
-       class="mt-7  md:w-40  w-24"
+  <img src="../assets/image/oculus.png" alt="sponsors"
+       class="mt-7 md:w-40 w-24"
   />
   
 </div>
